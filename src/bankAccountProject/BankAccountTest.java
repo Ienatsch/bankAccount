bf27ca778a529540142c07f6f88a7f0f871cbba7
Jake Maughan <jacobm2n527@gmail.com>
2019-03-16 19:55:39 -0500

    Created boiler plate

---
+++ src/bankAccountProject/BankAccountTest.java
@@ -0,0 +1,17 @@
+/** File Prologue
+ * Name: Jacob Maughan & Dolan Lenatsch
+ * Assignment: Final Project
+ * Date: 03/15/19 - 4/10/19
+ * CIT260
+ */
+package bankAccountProject;
+
+import java.util.Scanner;
+public class BankAccountTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/bankAccountProject/CheckingAccount.java
@@ -0,0 +1,148 @@
+/** File Prologue
+ * Name: Jacob Maughan & Dolan Lenatsch
+ * Assignment: Final Project
+ * Date: 03/15/19 - 4/10/19
+ * CIT260
+ */
+package bankAccountProject;
+
+public class CheckingAccount extends BankAccount {
+// Data Fields
+	private int routingNumber = 00001;
+	private double interestRate = .01;
+	private double value = 0;
+	
+// Constructors
+	/**	
+	 * The CheckingAcoount default constructor method
+	 * Purpose: Creates a CheckingAccount object that inherits the super data fields
+	 * @param void
+	 * @return void
+	 */
+	public CheckingAccount() {
+		super();
+	}
+	
+	/**	
+	 * The CheckingAcoount constructor method
+	 * Purpose: Creates a CheckingAccount object with parameters
+	 * @param int
+	 * @param double
+	 * @param double
+	 * @return void
+	 */
+	public CheckingAccount(int routingNumber, double interestRate, double value) {
+		this.routingNumber = routingNumber;
+		this.interestRate = interestRate;
+		this.value = value;
+	}
+	
+	/**	
+	 * The CheckingAcoount constructor method
+	 * Purpose: Creates a CheckingAccount object that with specified data fields
+	 * @param String
+	 * @param int
+	 * @param double
+	 * @param int
+	 * @param double 
+	 * @param double
+	 * @return void
+	 */
+	public CheckingAccount(String bankName, int accountNumber, double totalValue, int routingNumber, double interestRate, double value) {
+		super();
+		this.routingNumber = routingNumber;
+		this.interestRate = interestRate;
+		this.value = value;
+	}
+// Methods
+	/**	
+	 * The getRoutingNumber method
+	 * Purpose: gets the routingNumber
+	 * @param void
+	 * @return routingNumber
+	 */
+	public int getRoutingNumber() {
+		return routingNumber;
+	}
+	
+	/**	
+	 * The getInterestRate method
+	 * Purpose: gets the interestRate
+	 * @param void
+	 * @return interestRate
+	 */
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	/**	
+	 * The getValue method
+	 * Purpose: gets the value
+	 * @param void
+	 * @return value
+	 */
+	public double getValue() {
+		return value;
+	}
+	
+	/**	
+	 * The setRoutingNumber method
+	 * Purpose: sets the routingNumber
+	 * @param int
+	 * @return void
+	 */
+	public void setRoutingNumber(int routingNumber) {
+		this.routingNumber = routingNumber;
+	}
+	
+	/**	
+	 * The setInterestRate method
+	 * Purpose: sets the interestRate
+	 * @param double
+	 * @return void
+	 */
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	/**	
+	 * The setValue method
+	 * Purpose: sets the value
+	 * @param double
+	 * @return void
+	 */
+	public void setValue(double value) {
+		this.value = value;
+	}
+	
+	/**	
+	 * The calculateInterest method
+	 * Purpose: calculates the interest of the account
+	 * @param void
+	 * @return double
+	 */
+	public double calculateInterest() {
+		return value * interestRate;
+	}
+	
+	/**	
+	 * The writeToFile method
+	 * Purpose: writes the account information to a file
+	 * @param void
+	 * @return void
+	 */
+	public void writeToFile() {
+		
+	}
+	
+	/**	
+	 * The writeToFile method
+	 * Purpose: writes the account information to a specified file
+	 * @param void
+	 * @return void
+	 */
+	public void writeToFile(String fileLocation) {
+		
+	}
+
+}
