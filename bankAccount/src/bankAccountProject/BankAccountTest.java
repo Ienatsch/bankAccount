c02989c4aefa933e78446987668288a0717e5a9e
Dalan Ienatsch <dienatsch93@gmail.com>
2019-04-01 18:10:41 -0400

    ...

--- bankAccount/src/bankAccountProject/BankAccountTest.java
+++ bankAccount/src/bankAccountProject/BankAccountTest.java
@@ -6,13 +6,99 @@
  */
 package bankAccountProject;
 
-import java.util.Scanner;
+import java.util.*;
 import java.io.*;
 public class BankAccountTest {
 
 	public static void main(String[] args) {
 		// Declare the scanner 
 		Scanner input =  new Scanner(System.in);
+
+		// Create an ArrayList of references to BankAccount objects
+        ArrayList<BankAccount> bankAccounts = new ArrayList<BankAccount>();
+        
+		// Create one CheckingAccount object and store the reference to it in the ArrayList
+        BankAccount myChecking = new BankAccount();
+        bankAccounts.add(myChecking);
+        
+		// Create two SavingsAccount objects and store references to them in the ArrayList
+        BankAccount savings1 = new SavingsAccount();
+        BankAccount savings2 = new SavingsAccount();
+        bankAccounts.add(savings1);
+        bankAccounts.add(savings2);
+        
+        // Ask the user for the day of their deposit
+        // Or should we just get the current date one the program starts and parse through that?
+        
+        // Display a summary of the accounts so that the user can access them
+        System.out.println("-- Account Name ---- Account Number ---- Value ");
+        System.out.println("   " + myChecking.getBankName() + "           " + myChecking.getRoutingNumber() + "              " + myChecking.value() + " ");
+        System.out.println("   " + savings1.getBankName() + "           " + savings1.getRoutingNumber() + "              " + savings1.value() + " ");
+        System.out.println("   " + savings2.getBankName() + "           " + savings2.getRoutingNumber() + "              " + savings2.value() + " ");
+        
+        int selection = 0;
+		do {
+            System.out.println("Press 1 to Deposit");
+            System.out.println("Press 2 to Withdraw");
+            System.out.println("Press 3 to Exit");
+
+            selection = input.nextInt();
+            int accountNumber = 0;
+
+            switch (selection) {
+                case 1: // Deposit money in an account
+                    // ask the user to enter the account number and deposit amount
+                    System.out.println("Please enter the account number (123456) and deposit amount (350.00)");
+                    accountNumber = input.nextInt();
+                    double deposit = input.nextDouble();
+                    
+                    // add the deposit amount to the value
+                    if (accountNumber == 123456) {
+                    	myChecking.deposit(myChecking, deposit);
+                    	// Test output
+                    	System.out.println(myChecking.value());
+                    } else if (accountNumber == 234567) {
+                    	savings1.deposit(savings1, deposit);
+     
+                    } else if (accountNumber == 245678) {
+                    	savings2.deposit(savings2, deposit);
+                    } else
+                    	System.out.println("You did not enter a valid account number. Please try again.");
+                    break;
+                    
+                case 2: // Withdraw money out of an account
+                    // ask the user to enter the account number and withdraw amount
+                	System.out.println("Please enter the account number (123456) and withdraw amount (350.00)");
+                	accountNumber = input.nextInt();
+                    double withdraw = input.nextDouble();
+                    
+                    // subtract the withdraw amount from the value
+                    if (accountNumber == 123456) {
+                    	myChecking.withdraw(myChecking, withdraw);
+                    	// Test output
+                    	System.out.println(myChecking.value());
+                    } else if (accountNumber == 234567) {
+                    	savings1.withdraw(savings1, withdraw);
+                    } else if (accountNumber == 245678) {
+                    	savings2.withdraw(savings2, withdraw);
+                    } else
+                    	System.out.println("You did not enter a valid account number. Please try again.");
+                    break;
+            }
+            
+        } while (selection < 3);
+
+			
+		// Create a file on disk and for each account in the list write out
+		// The account name
+		// The account number
+		// The account balance
+		// The interest earned
+		// Close the file 
+		// Open the file 
+		// Output the headings on the console for the bank report
+		// Read in the data from the file on disk for each account
+		// Write the data for each account on the console
 
 	}
 
--- bankAccount/src/bankAccountProject/CheckingAccount.java
+++ bankAccount/src/bankAccountProject/CheckingAccount.java
@@ -8,141 +8,83 @@ package bankAccountProject;
 
 public class CheckingAccount extends BankAccount {
 // Data Fields
-	private int accountNumber = 00001;
-	private double interestRate = .01;
-	private double value = 0;
-	
+    private final int MAX_ACCOUNT = 5000000;
+    private final int MIN_ACCOUNT = 1000000;
+    private int accountNumber = (int) (Math.random() * MAX_ACCOUNT) + MIN_ACCOUNT;
+    
+    
 // Constructors
-	/**	
-	 * The CheckingAcoount default constructor method
-	 * Purpose: Creates a CheckingAccount object that inherits the super data fields
-	 * @param void
-	 * @return void
-	 */
-	public CheckingAccount() {
-		super();
-	}
-	
-	/**	
-	 * The CheckingAcoount constructor method
-	 * Purpose: Creates a CheckingAccount object with parameters
-	 * @param int
-	 * @param double
-	 * @param double
-	 * @return void
-	 */
-	public CheckingAccount(int accountNumber, double interestRate, double value) {
-		this.accountNumber = accountNumber;
-		this.interestRate = interestRate;
-		this.value = value;
-	}
-	
-	/**	
-	 * The CheckingAcoount constructor method
-	 * Purpose: Creates a CheckingAccount object that with specified data fields
-	 * @param String
-	 * @param int
-	 * @param double
-	 * @param int
-	 * @param double 
-	 * @param double
-	 * @return void
-	 */
-	public CheckingAccount(String bankName, int routingNumber, double totalValue, int accountNumber, double interestRate, double value) {
-		super();
-		this.accountNumber = accountNumber;
-		this.interestRate = interestRate;
-		this.value = value;
-	}
-// Methods
-	/**	
-	 * The getRoutingNumber method
-	 * Purpose: gets the routingNumber
-	 * @param void
-	 * @return routingNumber
-	 */
-	public int getAccountNumber() {
-		return accountNumber;
-	}
-	
-	/**	
-	 * The getInterestRate method
-	 * Purpose: gets the interestRate
-	 * @param void
-	 * @return interestRate
-	 */
-	public double getInterestRate() {
-		return interestRate;
-	}
+    /**
+     * The CheckingAcoount default constructor method Purpose: Creates a
+     * CheckingAccount object that inherits the super data fields
+     *
+     * @param void
+     * @return void
+     */
+    public CheckingAccount() {
+        super();
+    }
 
-	/**	
-	 * The getValue method
-	 * Purpose: gets the value
-	 * @param void
-	 * @return value
-	 */
-	public double getValue() {
-		return value;
-	}
-	
-	/**	
-	 * The setRoutingNumber method
-	 * Purpose: sets the routingNumber
-	 * @param int
-	 * @return void
-	 */
-	public void setRoutingNumber(int accountNumber) {
-		this.accountNumber = accountNumber;
-	}
-	
-	/**	
-	 * The setInterestRate method
-	 * Purpose: sets the interestRate
-	 * @param double
-	 * @return void
-	 */
-	public void setInterestRate(double interestRate) {
-		this.interestRate = interestRate;
-	}
-	
-	/**	
-	 * The setValue method
-	 * Purpose: sets the value
-	 * @param double
-	 * @return void
-	 */
-	public void setValue(double value) {
-		this.value = value;
-	}
-	
-	/**	
-	 * The calculateInterest method
-	 * Purpose: calculates the interest of the account
-	 * @param void
-	 * @return double
-	 */
-	public double calculateInterest() {
-		return value * interestRate;
-	}
-	
-	/**	
-	 * The writeToFile method
-	 * Purpose: writes the account information to a file
-	 * @param void
-	 * @return void
-	 */
-	public void writeToFile() {
-		
-	}
-	
-	/**	
-	 * The writeToFile method
-	 * Purpose: writes the account information to a specified file
-	 * @param void
-	 * @return void
-	 */
-	public void writeToFile(String fileLocation) {
-		
-	}
+    /**
+     * The CheckingAcoount constructor method Purpose: Creates a CheckingAccount
+     * object with parameters
+     *
+     * @param int
+     * @return void
+     */
+    public CheckingAccount(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+// Methods
+    /**
+     * The getRoutingNumber method Purpose: gets the routingNumber
+     *
+     * @param void
+     * @return routingNumber
+     */
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     * The getValue method Purpose: gets the value
+     *
+     * @param void
+     * @return value
+     */
+    public double getValue() {
+        return value;
+    }
+
+    /**
+     * The setValue method Purpose: sets the value
+     *
+     * @param double
+     * @return void
+     */
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    /**
+     * The readFromFile method Purpose: reads the account information from a file
+     *
+     * @param void
+     * @return void
+     */
+    public void readFromFile() {
+        
+    }
+    
+    /**
+     * The writeToFile method Purpose: writes the account information to a file
+     *
+     * @param void
+     * @return void
+     */
+    public void writeToFile() {
+
+    }
 
 }
